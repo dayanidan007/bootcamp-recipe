93665117d07bb01006ef594bb963f9eec700f70d
dayanidan007 <dayanidan007@gmail.com>
2020-11-24 11:15:30 +0200

    Finish

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+const api = require('./server/routes/api')
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use('/', api);
+
+
+
+const port = 8080
+app.listen(port, function () {
+    console.log('Running the server :)')
+})
